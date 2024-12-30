e278ce316f938eb5687de8c8e7ed95f8f2b8e908
Sri Krishna Chekuri <cskcvarma@outlook.com>
2024-12-30 12:31:07 -0500

    basic server

--- src/chat_app.rs
+++ src/chat_app.rs
@@ -1,10 +1,45 @@
-use std::net::{TcpListener};
-
-
-
-
 mod server {
+    use std::{net::{TcpListener, TcpStream}, thread, io::{Read, Write}};
     // server side logic will go here.
+    pub fn start() {
+        let port = "127.0.0.1:8080";
+        let listener = TcpListener::bind(port).unwrap();
+        println!("Server listening on {}", port);
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(move || handle_client(stream));
+                }
+                Err(e) => {
+                    println!("Error accepting connection: {}", e);
+                }
+                
+            }
+        }
+    }
+
+    fn handle_client(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(size) => {
+                    if size == 0 {
+                        println!("Client Disconnected");
+                        break;
+                    }
+
+                    stream.write(&buffer[..size]).unwrap();
+                }
+                Err(e) => {
+                    println!("Error reading from client: {}", e);
+                    break;
+                }
+            }
+        }
+
+    }
 }
 
 mod client {
@@ -12,7 +47,6 @@ mod client {
 }
 
 pub fn start() {
-    let port = "127.0.0.1:8080";
-    let listener = TcpListener::bind(port).unwrap();
-    println!("Server listening on {}", port)
+    server::start();
+    
 }
